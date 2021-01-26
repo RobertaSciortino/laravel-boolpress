ee3e71171363325a05da2faafc37b575bdbfa838
Roberta <robertasciortino.ag@gmail.com>
2021-01-26 15:49:49 +0100

    creazione gruppo di rotte admin

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+//gruppo di rotte admin
+Route::get('/home', 'Admin\HomeController@index')->name('dashboard')->middleware('auth')->namespace('admin')->prefix('admin')->group(function(){
+  Route::get('/home', 'HomeController@index')->name('home');
+});
