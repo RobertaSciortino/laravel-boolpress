9915731c5b42ef713d68f8aae8bb1b9afdc8c01e
Roberta <robertasciortino.ag@gmail.com>
2021-01-26 17:22:36 +0100

    creazione pagina contatti

---
+++ resources/views/guest/contatti.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h1>
+          Contatti
+        </h1>
+      </div>
+    </div>
+  </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index')->name('index');
+Route::get('/contatti', 'HomeController@contatti')->name('contatti');
 
 Auth::routes(['register' => false]);
 
