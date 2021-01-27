818a01d1e86550ab26656932de280574102d1f43
Roberta <robertasciortino.ag@gmail.com>
2021-01-27 16:15:49 +0100

    verifica post in admin show

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -70,6 +70,10 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
+      if(!$post){
+        abort(404);
+      }
+      
       $data = [
         'post' => $post
       ];
@@ -82,7 +86,7 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
         //
     }
