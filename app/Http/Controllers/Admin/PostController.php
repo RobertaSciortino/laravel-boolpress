83567b2b5f3d527a503de41029726591a0cb2ba0
Roberta <robertasciortino.ag@gmail.com>
2021-01-29 17:07:29 +0100

    delete sync admin

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -148,6 +148,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+      $post->tags()->sync([]);
+
       $post->delete();
 
       return redirect()->route('admin.posts.index');
