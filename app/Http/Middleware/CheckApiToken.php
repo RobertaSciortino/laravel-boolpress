3cf8890f51827ffd9013fa6a1dddb6f43c1ea934
Roberta <robertasciortino.ag@gmail.com>
2021-02-01 18:38:10 +0100

    verifica api token utente in middleware

--- app/Http/Middleware/CheckApiToken.php
+++ app/Http/Middleware/CheckApiToken.php
@@ -15,6 +15,25 @@ class CheckApiToken
      */
     public function handle($request, Closure $next)
     {
+      $auth_token = $request->header('authorization');
+      //verifico se e' presente un token di autorizzazione
+      if(empty($auth_token)){
+        return response()->json([
+          'success' => false,
+          'error' => 'API Token mancante'
+        ]);
+      }
+      //estraggo il token
+      $api_token = substr($auth_token, 7);
+
+      //verifico se l'api token e' corretto
+      $user = User::where('api_token', $api_token)->first();
+      if (!$user) {
+        return response()->json([
+          'success' => false,
+          'error' => 'API Token mancante'
+        ]);
+      }
         return $next($request);
     }
 }
