1999747604d5da8dca9d8d2ecfbce21911d5a811
Roberta <robertasciortino.ag@gmail.com>
2021-02-01 11:36:13 +0100

    modifica layout view edit

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,47 +1,57 @@
 @extends('layouts.dashboard')
 
 @section('content')
-  <table class="table">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Titolo</th>
-      <th scope="col">Slug</th>
-      <th scope="col">Azioni</th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach ($posts as $post)
-      <tr>
-        <td>
-          {{$post->id}}
-        </td>
-        <td>
-          {{$post->title}}
-        </td>
-        <td>
-          {{$post->slug}}
-        </td>
-        <td>
-          <a href="{{ route('admin.posts.show', ['post' => $post->id])}}" class="btn btn-info">
-            Visualizza
-          </a>
-          <a href="{{ route('admin.posts.create')}}" class="btn btn-primary">
-            Crea
-          </a>
-          <a href="{{ route('admin.posts.edit', ['post' => $post->id])}}" class="btn btn-warning">
-            Modifica
-          </a>
-          <form action="{{route('admin.posts.destroy', ['post' => $post->id])}}" method="post">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger">Elimina</button>
-          </form>
-        </td>
-      </tr>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-right">
+        <a href="{{ route('admin.posts.create')}}" class="btn btn-primary">
+          Crea un nuovo post
+        </a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Titolo</th>
+              <th scope="col">Slug</th>
+              <th scope="col">Azioni</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($posts as $post)
+              <tr>
+                <td>
+                  {{$post->id}}
+                </td>
+                <td>
+                  {{$post->title}}
+                </td>
+                <td>
+                  {{$post->slug}}
+                </td>
+                <td>
+                  <a href="{{ route('admin.posts.show', ['post' => $post->id])}}" class="btn btn-info">
+                    Visualizza
+                  </a>
+                  <a href="{{ route('admin.posts.edit', ['post' => $post->id])}}" class="btn btn-warning">
+                    Modifica
+                  </a>
+                  <form action="{{route('admin.posts.destroy', ['post' => $post->id])}}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Elimina</button>
+                  </form>
+                </td>
+              </tr>
 
-    @endforeach
+            @endforeach
 
-  </tbody>
-</table>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 @endsection
